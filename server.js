b31cef27c416044c3e853067dae4981a8a65ca70
Your Name <you@example.com>
2026-02-24 16:38:11 +0100

    cleaned up, created frontend/backend folder + created html, js & css file. + set up html boilerplate refs #3

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ db.prepare(`
 
 // Beispiel: Nutzer hinzufügen (Route)
 app.get('/register', (req, res) => {
-    const { username, password } = req.body;
-    const insert = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
-    insert.run(username, password);
-    res.send("Registriert!");
+  const { username, password } = req.body;
+  const insert = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+  insert.run(username, password);
+  res.send("Registriert!");
 });
 
 
 // Beispiel: Nutzer hinzufügen (Route)
 app.get('/users', (req, res) => {
-    console.log("Users wurde ausgeführt. Hier kommt funktionalität, so dass Benutzer zurückgesendet werden")
-    res.send("USERS!!");
+  console.log("Users wurde ausgeführt. Hier kommt funktionalität, so dass Benutzer zurückgesendet werden")
+  res.send("USERS!!");
 });
 
 app.listen(3000, () => console.log("Server läuft auf Port 3000"));
